5c65c74e2cc145bd993153112766e0179ae8698b
Victor Bicego <vbicego@evoila.de>
2022-01-14 18:22:57 +0100

    First commit

---
+++ src/main/java/de/evoila/personalAbteilung/PersonalAbteilungApplication.java
@@ -0,0 +1,13 @@
+package de.evoila.personalAbteilung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAbteilungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalAbteilungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/evoila/personalAbteilung/PersonalAbteilungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.evoila.personalAbteilung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalAbteilungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
